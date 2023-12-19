946cea3f3b014799c059abf93ba4bc3334c47a01
Lasterius <zrk.vlg@gmail.com>
2023-12-19 12:55:12 +0100

    fix sw fetch request

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,6 @@
 const staticCacheName = 'test-app-v1';
+const dynamicCacheName = 'dyn-app-v1';
+
 const assetUrls = [
   '/',
   '/index.html',
@@ -23,30 +25,59 @@ self.addEventListener('activate', (event) => {
   /* eslint-disable-next-line no-restricted-globals */
   event.waitUntil(self.clients.claim());
 });
+
+// fetch v1
+
 /* eslint-disable-next-line no-restricted-globals */
-self.addEventListener('fetch', function (event) {
-  if (navigator.onLine) {
-    const fetchRequest = event.request.clone();
-    return fetch(fetchRequest).then(function (response) {
-      if (!response || response.status !== 200 || response.type !== 'basic') {
-        return response;
-      }
+// self.addEventListener('fetch', function (event) {
+//   if (navigator.onLine) {
+//     const fetchRequest = event.request.clone();
+//     return fetch(fetchRequest).then(function (response) {
+//       if (!response || response.status !== 200 || response.type !== 'basic') {
+//         return response;
+//       }
 
-      const responseToCache = response.clone();
+//       const responseToCache = response.clone();
 
-      caches.open(staticCacheName).then(function (cache) {
-        cache.put(event.request, responseToCache);
-      });
+//       caches.open(staticCacheName).then(function (cache) {
+//         cache.put(event.request, responseToCache);
+//       });
 
-      return response;
-    });
-  } else {
-    event.respondWith(
-      caches.match(event.request).then(function (response) {
-        if (response) {
-          return response;
-        }
-      })
-    );
-  }
+//       return response;
+//     });
+//   } else {
+//     event.respondWith(
+//       caches.match(event.request).then(function (response) {
+//         if (response) {
+//           return response;
+//         }
+//       })
+//     );
+//   }
+
+//     // event.respondWith((async () => {
+//     //   const cachedResponse = await caches.match(event.request);
+//     //   if (cachedResponse) {
+//     //     return cachedResponse;
+//     //   }
+//     //   const response = await fetch(event.request);
+//     //   if (!response || response.status !== 200 || response.type !== 'basic') {
+//     //     return response;
+//     //   }
+//     //   if (ENABLE_DYNAMIC_CACHING) {
+//     //     const responseToCache = response.clone();
+//     //     const cache = await caches.open(dynamicCacheName)
+//     //     await cache.put(event.request, response.clone());
+//     //   }
+//     //   return response;
+// });
+
+// fetch v2
+/* eslint-disable-next-line no-restricted-globals */
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
 });
